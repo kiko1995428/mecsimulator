877137da60bf791de28b43fd74f24c3abaa20bc4
sugjchan <gakuenyuki0912@gmail.com>
2019-11-11 15:14:21 +0900

    created new test code

--- CloudletSimulator/simulator/model/test.py
+++ CloudletSimulator/simulator/model/test.py
@@ -1,6 +1,6 @@
 #edege_serverのテスト用プログラム
 
-from CloudletSimulator.simulator.model.edge_server import MEC_server, cover_range_serch
+from CloudletSimulator.simulator.model.edge_server import MEC_server, cover_range_search
 import pandas as pd
 import pickle
 #CSV読み込み
@@ -23,18 +23,27 @@ device_flag = False
 f = open('/Users/sugimurayuuki/Desktop/mecsimulator/CloudletSimulator/sumo/device.binaryfile', 'rb')
 devices = pickle.load(f)
 #適当にデバイス１つ目、時刻８の緯度経度を取得
-device_lon = float(devices[0].plan[8].x) #文字列型であったので、floatに変換
-device_lat = float(devices[0].plan[8].y)
+#device_lon = float(devices[0].plan[8].x) #文字列型であったので、floatに変換
+#device_lat = float(devices[0].plan[8].y)
+print(len(devices))
+num = len(devices)
 
-#オブジェクト作成
-for index, series in df.iterrows():
-    mec[index] = MEC_server(resource, index+1, server_type, series["lon"], series["lat"], cover_range) #０からカウントのため、1を＋
-    print("ID:", mec[index].name, ",", "type:", mec[index].server_type, ",", "resource:", mec[index].resource)
-    print("lat:", mec[index].lat, ",", "lon:", mec[index].lon)
-    print("range:", mec[index].range,"m")
-    #ここで基地局のカバー範囲内にあるか判定する。
-    device_flag, memo = cover_range_serch(device_flag, device_lon, device_lat, mec[index].lon, mec[index].lat, cover_range, index + 1)
-    if memo > 0:
-        tmp = memo
-    print("**************")
-print("correct ID is ", tmp)
+for i in range(num):
+    device_lon = float(devices[i].plan[1].x)
+    device_lat = float(devices[i].plan[1].y)
+    for index, series in df.iterrows():
+        mec[index] = MEC_server(resource, index + 1, server_type, series["lon"], series["lat"],
+                                cover_range)  # ０からカウントのため、1を＋
+        #print("ID:", mec[index].name, ",", "type:", mec[index].server_type, ",", "resource:", mec[index].resource)
+        #print("lat:", mec[index].lat, ",", "lon:", mec[index].lon)
+        #print("range:", mec[index].range, "m")
+        # ここで基地局のカバー範囲内にあるか判定する。
+        memo = cover_range_search(device_flag, device_lon, device_lat, mec[index].lon, mec[index].lat,
+                                              cover_range, index + 1)
+        if memo > 0:
+            tmp = memo
+        #print("**************")
+    print("車両ID：", i, ",", "correct ID is ", tmp)
+
+
+
