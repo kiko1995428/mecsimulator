5bd2b0f96fe3a0dab481fa0b936e8f3969fa7aa6
sugjchan <gakuenyuki0912@gmail.com>
2019-11-20 22:20:58 +0900

    added Mec_name

--- CloudletSimulator/simulator/model/angle.py
+++ CloudletSimulator/simulator/model/angle.py
@@ -5,3 +5,4 @@ from typing import List
 
 Angle = collections.namedtuple('Angle', ('t', 'angle'))
 Speed = collections.namedtuple('Speed', ('t', 'speed'))
+Mec_name = collections.namedtuple('Mec_name', ('t', 'mec_name'))
